9633995ed229231cdc6cf9a92ad94bc891092031
Nhật Bằng <nhatbang496@gmail.com>
2025-11-11 17:10:08 +0700

    Thêm tính năng Quản lý làm việc và 2 model liên quan

---
+++ QuanLyLamViec/Quanlylamviec.cs
@@ -0,0 +1,67 @@
+﻿using GZone.models; // Thêm
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GZone.QuanLyLamViec
+{
+    public partial class Quanlylamviec : Form
+    {
+        private NhanVienDAL _nhanVienDAL;
+
+        public Quanlylamviec()
+        {
+            InitializeComponent();
+            _nhanVienDAL = new NhanVienDAL();
+        }
+
+        private void Quanlylamviec_Load(object sender, EventArgs e)
+        {
+            LoadDanhSachNhanVien();
+        }
+
+        private void LoadDanhSachNhanVien()
+        {
+            // Lấy danh sách NV (kèm tên chi nhánh)
+            List<NhanVien> dsNhanVien = _nhanVienDAL.GetAllNhanVienWithChiNhanh();
+            dgvNhanVien.DataSource = dsNhanVien;
+
+            // Cấu hình cột
+            dgvNhanVien.Columns["Ma"].Visible = false; // Ẩn Mã NV
+            dgvNhanVien.Columns["MaChiNhanh"].Visible = false; // Ẩn Mã CN
+
+            dgvNhanVien.Columns["Ten"].HeaderText = "Tên Nhân Viên";
+            dgvNhanVien.Columns["Ten"].Width = 200;
+            dgvNhanVien.Columns["Sdt"].HeaderText = "Số Điện Thoại";
+            dgvNhanVien.Columns["GioiTinh"].HeaderText = "Giới Tính";
+            dgvNhanVien.Columns["TenChiNhanh"].HeaderText = "Chi Nhánh";
+
+            dgvNhanVien.ReadOnly = true;
+            dgvNhanVien.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvNhanVien.MultiSelect = false;
+            dgvNhanVien.AllowUserToAddRows = false;
+        }
+
+        private void dgvNhanVien_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                // Lấy nhân viên được chọn từ dòng
+                NhanVien selectedNhanVien = dgvNhanVien.Rows[e.RowIndex].DataBoundItem as NhanVien;
+
+                if (selectedNhanVien != null)
+                {
+                    // Mở form xếp lịch và truyền nhân viên qua
+                    frmXepLichNhanVien formXepLich = new frmXepLichNhanVien(selectedNhanVien);
+                    formXepLich.ShowDialog();
+                }
+            }
+        }
+    }
+}
---
+++ models/Ca.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace GZone.models
+{
+    // Model cho bảng CA
+    public class Ca
+    {
+        public string MaCa { get; set; }
+        public string TenCa { get; set; }
+        public TimeSpan ThoiGianBD { get; set; }
+        public TimeSpan ThoiGianKT { get; set; }
+    }
+
+    // DAL để truy vấn bảng CA
+    public class CaDAL
+    {
+        public List<Ca> GetAllCa()
+        {
+            List<Ca> list = new List<Ca>();
+            if (clsDatabase.OpenConnection())
+            {
+                try
+                {
+                    // Sắp xếp theo giờ bắt đầu để "Sáng -> Chiều -> Tối"
+                    string query = "SELECT * FROM dbo.CA_LAM_VIEC ORDER BY CA_ThoiGianBD";
+                    SqlCommand cmd = new SqlCommand(query, clsDatabase.con);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        list.Add(new Ca
+                        {
+                            MaCa = reader["CA_Ma"].ToString(),
+                            TenCa = reader["CA_Ten"].ToString(),
+                            ThoiGianBD = (TimeSpan)reader["CA_ThoiGianBD"],
+                            ThoiGianKT = (TimeSpan)reader["CA_ThoiGianKT"]
+                        });
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex) { MessageBox.Show("Lỗi khi tải danh sách ca: " + ex.Message); }
+                finally { clsDatabase.CloseConnection(); }
+            }
+            return list;
+        }
+    }
+}
